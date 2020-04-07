591a7a9235285c71e4e20c5549cb8c143daea191
Vlad <rakin_vk@smart-transport.ru>
2020-04-07 19:13:05 +0500

    Add admin panel

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,32 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import *
+
+
+@admin.register(Bill)
+class BillAdmin(admin.ModelAdmin):
+    list_display = ('user', 'name', 'sum', 'created_date', )
+    list_filter = ('user', )
+
+
+@admin.register(OperationType)
+class OperationTypeAdmin(admin.ModelAdmin):
+    list_display = ('name', )
+    list_filter = ('name', )
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'operation_type', )
+    list_filter = ('name', 'operation_type', )
+
+
+@admin.register(Transaction)
+class TransactionAdmin(admin.ModelAdmin):
+    list_display = ('user', 'get_operation_type', 'category', 'bill', 'sum', 'date', 'tag', 'comment', )
+    list_filter = ('user', 'category__operation_type', 'category', 'bill', 'date', 'tag', )
+
+    def get_operation_type(self, obj):
+        return obj.category.operation_type.get_name_display()
+
+    get_operation_type.short_description = 'Вид операции'
--- core/models.py
+++ core/models.py
@@ -14,26 +14,35 @@ class Bill(models.Model):
         verbose_name = 'Счет'
         verbose_name_plural = 'Счета'
 
+    def __str__(self):
+        return self.name
 
-class Action(models.Model):
+
+class OperationType(models.Model):
     ACTION_CHOICES = (
         ('income', 'доход'),
         ('expense', 'расход'),
     )
-    name = models.CharField(verbose_name='Название', max_length=50, choices=ACTION_CHOICES)
+    name = models.CharField(verbose_name='Название', max_length=50, choices=ACTION_CHOICES, unique=True)
 
     class Meta:
-        verbose_name = 'Действие с деньгами'
-        verbose_name_plural = 'Действия с деньгами'
+        verbose_name = 'Вид операции'
+        verbose_name_plural = 'Виды операции'
+
+    def __str__(self):
+        return self.get_name_display()
 
 
 class Category(models.Model):
-    action = models.ForeignKey(Action, on_delete=models.CASCADE)
+    operation_type = models.ForeignKey(OperationType, on_delete=models.CASCADE)
     name = models.CharField(verbose_name='Название', max_length=255)
 
     class Meta:
         verbose_name = 'Категория'
         verbose_name_plural = 'Категории'
+
+    def __str__(self):
+        return self.name
 
 
 class Transaction(models.Model):
@@ -43,8 +52,8 @@ class Transaction(models.Model):
 
     date = models.DateTimeField(verbose_name='Дата операции', default=timezone.now)
     sum = models.DecimalField(verbose_name='Сумма', max_digits=30, decimal_places=2)
-    tag = models.CharField(verbose_name='Тег', max_length=255, null=True)
-    comment = models.TextField(verbose_name='Комментарий', null=True)
+    tag = models.CharField(verbose_name='Тег', max_length=255, null=True, blank=True)
+    comment = models.TextField(verbose_name='Комментарий', null=True, blank=True)
 
     class Meta:
         verbose_name = 'Операция'
