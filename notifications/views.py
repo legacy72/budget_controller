912114f0c48507201adbfc2ae6a718af464548c9
legacy72 <sky45leg72>
2020-06-23 19:19:43 +0500

    Add reduce_expense and budget_expiration notifications

--- notifications/views.py
+++ notifications/views.py
@@ -1,27 +1,75 @@
-from collections import Counter
-
-from django.db.models import Q
 from django.utils import timezone
-from rest_framework import status
-from rest_framework import filters
 from rest_framework import viewsets
 from rest_framework.response import Response
-from rest_framework.decorators import api_view
-from url_filter.integrations.drf import DjangoFilterBackend
 
 from core.models import (
-    User, Bill, Category, Transaction, PlannedBudget, AuthCode
+    User, Bill, Category, Transaction, PlannedBudget
 )
 from budget_controller.utils.money import round_digit
 
 
 class ReduceExpenseViewSet(viewsets.ViewSet):
     """
-    Mock
+    Вьюшка для уведомлений, которые показывают что нужно сбавить расходы, если текущие расходы превышают средний
+    показатель
+
+    need_reduce_costs - нужно ли сбавить расходы (true/false)
+    expense_deviation_percentage - процент отклонения от среднего расхода
+                                   (если число положительное, то отклонение в плюс, если отрицательное - то в минус)
     """
 
     def list(self, request):
-        return Response(True)
+        # TODO: в данной реализации не учитываются месяцы, в которых не было совершенно тарнзакций (возможно стоит)
+        user = self.request.user.id
+
+        statistic = []
+
+        transactions = Transaction.objects\
+            .filter(
+                user=user,
+            )\
+            .select_related('bill', 'category', 'category__operation_type')\
+            .order_by('date')
+        month_statistic = {
+            'income': 0,
+            'expense': 0,
+            'month': transactions[0].date.month,
+            'year': transactions[0].date.year,
+        }
+        income = 0
+        expense = 0
+        count_months = 0
+        for transaction in transactions:
+            # если новый месяц наступил в списке транзакций, то создаем новый словарь на следующий месяц, а этот месяц
+            # записываем в массив
+            if transaction.date.month != month_statistic['month'] or transaction.date.year != month_statistic['year']:
+                statistic.append(month_statistic)
+                month_statistic = {
+                    'income': 0,
+                    'expense': 0,
+                    'month': transaction.date.month,
+                    'year': transaction.date.year,
+                }
+                count_months += 1
+
+            if transaction.category.operation_type.name == 'income':
+                month_statistic['income'] += transaction.sum
+                income += transaction.sum
+            if transaction.category.operation_type.name == 'expense':
+                month_statistic['expense'] += transaction.sum
+                expense += transaction.sum
+        # последний месяц тоже записываем в массив
+        statistic.append(month_statistic)
+        count_months += 1
+
+        average_expense = round_digit(expense / count_months)
+
+        average_deviation = {
+            'need_reduce_costs': True if int(month_statistic['expense'] * 100 / average_expense) - 100 > 0 else False,
+            'expense_deviation_percentage': int(month_statistic['expense'] * 100 / average_expense) - 100,
+        }
+
+        return Response(average_deviation)
 
 
 class AverageDeviationViewSet(viewsets.ViewSet):
@@ -98,8 +146,70 @@ class AverageDeviationViewSet(viewsets.ViewSet):
 
 class BudgetExpirationViewSet(viewsets.ViewSet):
     """
-    Mock
+    Уведомление о том, что по какой-либо категории скоро закончится бюджет.
+    Определеятся это так: если процент остатка от планируемого бюджета меньше 10 процентов и больше 0, то значит, что
+    бюджет скоро закончится по данной категории
+    
+    category - id категории
+    category_name - название категории
+    operation_type - id типа операции
+    operation_type_name - название операции
+    planed - планировано было заработать/потратить
+    fact - фактически было заработано/потрачено
+    balance - остаток (если отрицательный у категории дохода, то значит перерасход, если у прибыли - то заработано
+                       больше чем планировалось)
+    balance_percent - процент оставшейся суммы от планируемого бюджета
+    soon_expiration - истечет ли скоро бюджет (true/false)
     """
 
     def list(self, request):
-        return Response(True)
+        user = self.request.user.id
+        month = timezone.now().month
+        year = timezone.now().year
+
+        planned_budget = PlannedBudget.objects.filter(
+            user=user,
+            date__month=month,
+            date__year=year,
+            category__operation_type__name='expense',
+        ).select_related('category', 'category__operation_type')
+
+        fact_budget = []
+        for planned_budget_by_category in planned_budget:
+            transactions_by_category = Transaction.objects.filter(
+                user=user,
+                date__month=month,
+                date__year=year,
+                category_id=planned_budget_by_category.category_id,
+            ).select_related('bill', 'category', 'category__operation_type')
+
+            fact_budget_by_category = {
+                'category': planned_budget_by_category.category.id,
+                'category_name': planned_budget_by_category.category.name,
+                'operation_type': planned_budget_by_category.category.operation_type.id,
+                'operation_type_name': planned_budget_by_category.category.operation_type.name,
+                'planed': planned_budget_by_category.sum,  # сколько планируется заработать/потратить
+                'fact': 0,  # сколько по факту заработал/потратил
+                'balance': planned_budget_by_category.sum,  # сколько осталось заработать/потратить
+                'balance_percent': 0,  # сколько осталось от планируемого бюджета в процентах
+                'soon_expiration': False,
+            }
+
+            for tranz in transactions_by_category:
+                fact_budget_by_category['balance'] -= tranz.sum
+                fact_budget_by_category['fact'] += tranz.sum
+
+            # если планирумый бюджет не 0 (избегаем деление на 0), то считаем процент оставшейся суммы от план. бюджета
+            if fact_budget_by_category['planed']:
+                fact_budget_by_category['balance_percent'] = round_digit(
+                    fact_budget_by_category['balance'] / fact_budget_by_category['planed'] * 100
+                )
+
+            # если процент остатка от планируемого бюджета меньше 10 процентов и больше 0, то значит, что
+            # бюджет скоро закончится по данной категории
+            if 0 < fact_budget_by_category['balance_percent'] < 10:
+                fact_budget_by_category['soon_expiration'] = True
+
+            fact_budget.append(fact_budget_by_category)
+
+        return Response(fact_budget)
